7ced590508cd216a806af11e1317194891c9bb12
NirmayK <nirmaykataria@gmail.com>
2024-10-26 15:32:09 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendString("Hi")
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendString("!ERROR!")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(11)
